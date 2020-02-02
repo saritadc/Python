933e3d8b916d6d2dafccc7ad5503c5a411e17ab1
saritadc <dsarritta@gmail.com>
2020-02-02 12:03:01 +0545

    automation file deleted

--- python_refresher/automation.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get('https://youtube.com')
-searchbox = driver.find_element_by_xpath('//*[@id="search"]')
-searchbox.send_keys('Python tutorial')
-searchButton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
-searchButton.click()
